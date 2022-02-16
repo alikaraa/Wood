654e8f3b0c2ba17135a26f256f7a7ac34ce435cb
Ali Kara <56256760+alikaraa@users.noreply.github.com>
2022-02-16 17:59:52 +0300

    Wood

---
+++ Assets/Scripts/Coll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coll : MonoBehaviour
+{
+    public int index;
+
+    BoxCollider boxCollider;
+
+    private void Awake(){
+        boxCollider = GetComponent<BoxCollider>();
+        index = transform.GetSiblingIndex();
+    }
+
+    public void HitCollider(float damage){
+        if(boxCollider.size.y - damage > 0.0f)
+            boxCollider.size = new Vector3(boxCollider.size.x, boxCollider.size.y - damage, boxCollider.size.z);
+        else
+            Destroy(this);
+    }
+    
+}
---
+++ Assets/Scripts/Wood.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Wood : MonoBehaviour
+{
+    [SerializeField] private SkinnedMeshRenderer skinnedMeshRenderer;
+    [SerializeField] private Transform woodTransform;
+    [SerializeField] private Vector3 rotationVector;
+    [SerializeField] private float rotationDuration;
+    
+
+    private void Start(){
+        woodTransform
+            .DOLocalRotate(rotationVector, rotationDuration, RotateMode.FastBeyond360)
+            .SetLoops(-1,LoopType.Restart)
+            .SetEase(Ease.Linear);
+    }
+
+    public void Hit(int keyIndex, float damage){
+        float colliderHeight = 2.399264f;
+        float newWeight = skinnedMeshRenderer.GetBlendShapeWeight(keyIndex) + damage * (100f / colliderHeight);
+        skinnedMeshRenderer.SetBlendShapeWeight(keyIndex, newWeight);
+    }
+
+}
